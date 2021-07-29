1370cdca1bac8e8ee02b2814c84c81ae23af0884
Ash <ashe.null@protonmail.ch>
2021-07-29 18:33:43 +0000

    add Thunderstore files

---
+++ pack.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+final=$1
+if [[ -z "$final" ]]; then
+	echo Final destination required
+	exit 1
+fi
+
+tmp=$(mktemp -d)
+patchers=$tmp/patchers
+dest=$tmp/dest.zip
+
+mkdir -p "$patchers"
+
+cp README.md Deliter/manifest.json "$tmp/"
+cp media/icon/256.png "$tmp/icon.png"
+cp Deliter/bin/Release/net35/{Deliter,Newtonsoft.Json}.dll "$patchers/"
+cp libs/YamlDotNet_UnitySubset35.dll "$patchers/YamlDotNet.dll"
+
+pushd "$tmp"
+zip -9r "$dest" .
+popd
+
+mv "$dest" "$final"
+
+rm -rf "$tmp"
